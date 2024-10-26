eba6b46e9400fa2e8f51c6223d909779e8766519
Kevin <kevin005005@gmail.com>
2024-10-26 17:24:27 +0200

    Logging added

--- session_2024_10_26/hangman/game.py
+++ session_2024_10_26/hangman/game.py
@@ -1,8 +1,11 @@
 import json
+import logging
 from random import choice
 import os
 
 import requests
+
+logger = logging.getLogger(__name__)
 
 
 def clear():
@@ -12,6 +15,7 @@ def clear():
 class Game:
     @staticmethod
     def load_steps(path='steps.json'):
+        logger.info('Loading steps.')
         with open(path, encoding='utf-8') as file:
             steps = json.load(file)
             return ['\n'.join(step) for step in steps]
@@ -19,13 +23,16 @@ class Game:
     @staticmethod
     def load_words(source='https://raw.githubusercontent.com/bevacqua/correcthorse/refs/heads/master/wordlist.json',
                    min_word_length=4):
+        logger.info('Loading words.')
         return [word for word in requests.get(source).json() if len(word) >= min_word_length]
 
     def __init__(self):
         self.steps = Game.load_steps()
         self.words = Game.load_words()
+        logger.debug(f'{len(self.steps)} steps, {len(self.words)} words loaded.')
 
     def start(self):
+        logger.info('Game started.')
         selected_word = choice(self.words).upper()
         print(selected_word)
         correct_guesses = []
@@ -37,23 +44,31 @@ class Game:
             print(' '.join(c if c in correct_guesses else '_' for c in selected_word))
             print('Incorrect: ', ', '.join(incorrect_guesses))
             while True:
+                logger.info('Prompting the user.')
                 guess = input('Enter a letter: ').upper()
+                logger.debug(f'User entered: "{guess}"')
                 if guess in incorrect_guesses or guess in correct_guesses:
                     print(f'The letter "{guess}" has already been guessed.')
+                    logger.error('Already existing guess entered.')
                     continue
                 elif not ('A' <= guess <= 'Z') or len(guess) > 1:
                     print(f'"{guess}" is not a letter')
+                    logger.error('Non-letter character entered.')
                     continue
                 else:
                     break
 
             if guess in selected_word:
                 correct_guesses.append(guess)
+                logger.info('Correct guess.')
             else:
                 incorrect_guesses.append(guess)
+                logger.info('Incorrect guess.')
 
         if len(incorrect_guesses) == len(self.steps) - 1:
             print(self.steps[len(incorrect_guesses)])
             print('Gamer over!')
+            logger.info('Defeat.')
         else:
+            logger.info('Victory.')
             print('Victory!')
--- session_2024_10_26/hangman/menu.py
+++ session_2024_10_26/hangman/menu.py
@@ -2,6 +2,18 @@ import json
 from random import choice
 
 from game import Game
+import logging
+import os
+import datetime as dt
 
-g = Game()
-g.start()
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+
+NOW = dt.datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
+
+logging.basicConfig(filename=f'logs/{NOW}.log', filemode='w', encoding='utf-8', datefmt='%Y-%m-%dT%H:%M:%S%z',
+                    level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(module)s - %(message)s')
+
+if __name__ == '__main__':
+    g = Game()
+    g.start()
